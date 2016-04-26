a947647784ea74bdf475c5d680735d900108410b
Lance Bailey <lance.bailey@easytobook.com>
2016-04-26 23:21:56 +0300

    Added Element Controller Added Page Controller Added custom Element Exception Added initial composer.json file

---
+++ Controller/PageController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bailz777
+ * Date: 26/04/2016
+ * Time: 22:08
+ */
+
+namespace Beyerz\SimpleHMVCBundle\Controller;
+
+
+abstract class PageController extends ElementController
+{
+
+}
---
+++ Exception/ElementException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bailz777
+ * Date: 26/04/2016
+ * Time: 22:04
+ */
+
+namespace Beyerz\SimpleHMVCBundle\Exception;
+
+
+class ElementException extends \Exception
+{
+
+}
